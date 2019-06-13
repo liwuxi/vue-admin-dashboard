4a0bee5062fb8515b224a9bf48945c56c89ed729
liwuxi <imwuxi@gmail.com>
2019-06-14 00:12:51 +0800

    Add Team pages

--- src/views/SignInFlow/SignIn.vue
+++ src/views/SignInFlow/SignIn.vue
@@ -50,40 +50,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.light-background {
-  background-color: $light-gray;
-}
-
-.dark-background {
-  background-color: $dark-blue;
-}
-
-.light-text {
-  color: $white;
-}
-
-.dark-text {
-  color: $black;
-}
-
-.light-field {
-  background: rgba(255, 255, 255, 0.2);
-  border: 1px solid rgba(255, 255, 255, 0.2);
-
-  &::placeholder {
-    color: rgba(255, 255, 255, 0.3);
-  }
-}
-
-.dark-field {
-  background: rgba(198, 208, 235, 0.2);
-  border: 1px solid rgba(0, 0, 0, 0.2);
-
-  &::placeholder {
-    color: rgba(0, 0, 0, 0.3);
-  }
-}
-
 .container {
   display: flex;
   justify-content: center;
@@ -93,62 +59,6 @@ export default {
 
 .logo {
   width: 400px;
-}
-
-h4 {
-  margin: 0;
-  font-weight: bold;
-  font-size: 24px;
-  line-height: 34px;
-  /* identical to box height */
   text-align: center;
-  color: #ffffff;
-}
-
-input {
-  height: 60px;
-  width: 100%;
-  background: rgba(255, 255, 255, 0.2);
-  border: 1px solid rgba(255, 255, 255, 0.2);
-  box-sizing: border-box;
-  border-radius: 4px;
-
-  font-size: 20px;
-  color: white;
-  padding-left: 20px;
-  margin-top: 20px;
-
-  &::placeholder {
-    color: rgba(255, 255, 255, 0.3);
-  }
-}
-
-button {
-  height: 60px;
-  width: 100%;
-  margin-top: 20px;
-  margin-bottom: 40px;
-  background: #56ccf2;
-  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
-  border-radius: 4px;
-  border: none;
-
-  font-size: 20px;
-  color: white;
-}
-
-a {
-  font-size: 16px;
-  line-height: 25px;
-  text-align: center;
-  text-decoration: none;
-}
-
-.light-link {
-  color: rgba(255, 255, 255, 0.3);
-}
-
-.dark-link {
-  color: rgba(0, 0, 0, 0.3);
 }
 </style>
